30a35f7a4c05a80f6d22325977add608036bfea0
Creeper59-code <lichtremi@gmail.com>
2026-03-24 00:08:26 -0600

    picture added

---
+++ HOMEWORK 9/thefunctions.js
@@ -0,0 +1,33 @@
+ function setup()
+            {
+                createCanvas(300,600);
+            }
+            function draw()
+            {
+                background(220);
+                fill(100,100,100);
+                ellipse(150,240,125,65);
+                fill(210,180,140);
+                circle(150,200,75);
+                fill(250,250,250);
+                ellipse(125,200,50,15);
+                ellipse(175,200,50,15);
+                fill(0,0,250)
+                circle(105,200,10)
+                circle(155,200,10)
+                fill(0,0,0);
+                line(145,225,175,215);
+                line(180,210,175,215);
+                square(100,240,100);
+                rect(115,175,70,10);
+                point(105,200);
+                point(155,200);
+                textSize(20);
+                text('Teenage hoodie master',0,50);
+                text('Remington Licht',50,550);
+                fill(150,75,0);
+                triangle(115,200,120,155,145,145);
+                triangle(125,200,130,155,155,145);
+                triangle(175,200,185,155,155,145);
+                triangle(155,200,165,155,135,145);
+            }

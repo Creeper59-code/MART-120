b900fb9f4acc100f9c25005e36295a608c2d9707
Creeper59-code <lichtremi@gmail.com>
2026-04-13 21:50:32 -0600

    square dude updated

---
+++ HOMEWORK 12/squaredude_home2.js
@@ -0,0 +1,145 @@
+
+var xd = 0
+var yd = 0
+var i = 0
+var x1 = 250
+var x1s = Math.floor(Math.random() * 2) + 1;
+var x2 = 250
+var x2s = Math.floor(Math.random() * 2) + 1;
+var y1 = 250
+var y1s = Math.floor(Math.random() * 2) + 1;
+var y2 = 250
+var y2s = Math.floor(Math.random() * 2) + 1;
+var mx = -50
+var my = -50
+
+function setup()
+{
+    createCanvas (500,500);
+    
+
+}
+
+function draw()
+{
+    background(50);
+        borders();
+        squareDude();
+        exit();
+        obstacles();
+        MouseObstacle();
+        obstacleBounce();
+        exitMessage();
+
+}
+
+function borders()
+{
+    //creates borders
+    fill(0,0,0)
+        // top border
+    rect(0,0,width,5);
+    // left border
+    rect(0,0,5,height);
+    // bottom border
+    rect(0, height-5,width-25, 5);
+    // right upper border
+    rect(width-5,0,5,height-25);
+}
+
+function squareDude()
+{
+    // square dude :)
+    fill(0,250,0);
+    square(xd,yd,25);
+}
+
+function exit()
+{
+    // exit
+    fill(250,250,250);
+    rect(475,475,50,75);
+}
+
+function obstacles()
+{
+    // obstacles
+    fill(250,0,0);
+    square(x1,y1,50);
+    circle(x2,y2,35);
+}
+
+function MouseObstacle()
+{
+    //mouse obstacle
+    fill(0,250,0);
+    square(mx, my, 20);
+    fill(0,0,250);
+    square((mx+5), (my+5), 10);
+}
+
+function obstacleBounce()
+{
+    // obstacles bounce
+    if(x1 <= 0 || x1 >= 450)
+        {
+            x1s *= -1;
+        }
+
+    if(y1 <= 0 || y1 >= 450)
+        {
+            y1s *= -1;
+        }
+
+     if(x2 <= 17.5 || x2 >= 482.5)
+        {
+            x2s *= -1;
+        }
+
+    if(y2 <= 17.5 || y2 >= 482.5)
+        {
+            y2s *= -1;
+        }
+    
+    x1 += x1s
+    y1 += y1s
+    x2 -= x2s
+    y2 -= y2s
+}
+
+//exit function
+function exitMessage()
+ {   
+    if (xd >= 475 && yd >= 475)
+    {
+        text("You Win!", width/2+25, height/2+25);
+    }
+ }
+
+function keyPressed()
+{
+   if (key == 'd')
+   {
+    xd += 12.5;
+   }
+   else if (key == 'a')
+   {
+    xd -= 12.5;
+   }
+   else if (key == 'w')
+   {
+    yd -= 12.5;
+   }
+   else if (key == 's')
+   {
+    yd += 12.5;
+   }
+}
+
+function mousePressed()
+{
+    mx = mouseX - 10
+    my = mouseY - 10
+}
+
+
